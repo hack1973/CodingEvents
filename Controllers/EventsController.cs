7a06a4c5ccc2254efe2508e3d6a40f02bd92a622
Michael Hacker <hack1973@swbell.net>
2020-11-13 15:38:32 -0600

    completed CH13.6 walk through

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+
+        //GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Events.Add("Strange Loop");
+            Events.Add("Grace Hopper");
+            Events.Add("Code with Pride");
+
+            ViewBag.events = Events;
+
+            return View();
+        }
+    }
+}
