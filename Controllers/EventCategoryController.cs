dcb9cfe7edd4ca798dee4b63e63e763390e8c923
Michael Hacker <hack1973@swbell.net>
2020-11-21 15:37:13 -0600

    Completed CH19 exercises

---
+++ Controllers/EventCategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        //GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<EventCategory> categories = context.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
